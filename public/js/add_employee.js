26c52a80111ac6ce61bcfac006f6091452ee8969
Tyler Gebel <gebelt@flip2.engr.oregonstate.edu>
2023-08-02 15:01:14 -0700

    Added public file with css, img, and js folder with add_employee.js in the js folder. Added to app.js and index.hbs to get form to add employee working. Need to fix employee ID insertions.

---
+++ public/js/add_employee.js
@@ -0,0 +1,107 @@
+// Get the objects we need to modify
+let addEmployeeForm = document.getElementById('add-employee-form-ajax');
+
+// Modify the objects we need
+addEmployeeForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputFirstName = document.getElementById("input-firstName");
+    let inputLastName = document.getElementById("input-lastName");
+    let inputPhoneNum = document.getElementById("input-phoneNum");
+    let inputHourlyWage = document.getElementById("input-hourlyWage");
+    let inputWorkEmail = document.getElementById("input-workEmail");
+
+
+    // Get the values from the form fields
+    let firstNameValue = inputFirstName.value;
+    let lastNameValue = inputLastName.value;
+    let phoneNumValue = inputPhoneNum.value;
+    let hourlyWageValue = inputHourlyWage.value;
+    let workEmailValue = inputWorkEmail.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        phoneNum: phoneNumValue,
+        hourlyWage: hourlyWageValue,
+        workEmail: workEmailValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-employee-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputFirstName.value = '';
+            inputLastName.value = '';
+            inputPhoneNum.value = '';
+            inputHourlyWage.value = '';
+            inputWorkEmail.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("employee-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let employeeIDCell = document.createElement("TD");
+    let firstNameCell = document.createElement("TD");
+    let lastNameCell = document.createElement("TD");
+    let phoneNumCell = document.createElement("TD");
+    let hourlyWageCell = document.createElement("TD");
+    let workEmailCell = document.createElement("TD");
+
+
+    // Fill the cells with correct data
+    employeeIDCell.innerText = newRow.id;
+    firstNameCell.innerText = newRow.firstName;
+    lastNameCell.innerText = newRow.lastName;
+    phoneNumCell.innerText = newRow.phoneNum;
+    hourlyWageCell.innerText = newRow.hourlyWage;
+    workEmailCell.innerText = newRow.workEmail;
+
+    // Add the cells to the row 
+    row.appendChild(employeeIDCell);
+    row.appendChild(firstNameCell);
+    row.appendChild(lastNameCell);
+    row.appendChild(phoneNumCell);
+    row.appendChild(hourlyWageCell);
+    row.appendChild(workEmailCell);
+    
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
